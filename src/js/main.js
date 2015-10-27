5c824543e9d4f971875d8058738abd34b2d5e3a5
Jasmine Baik <boshnivolo@gmail.com>
2015-10-27 16:19:41 -0400

    Remove unused tab JavaScript

--- src/js/main.js
+++ src/js/main.js
@@ -31,24 +31,3 @@
 
 
   })(); //END IIFE
-
-// START tabs
-
-$('.tabs').each(function(){ //iterates through each ahref
- $('.tab-content').each(function(){ //iterates through the corresponding content
-
- $('a').on('click', function(event){
-   event.preventDefault(); //stop browser to take action for clicked
-
-   var active = $(this).attr('href'); //targets active tab and grabs that href
-
-   $(this).add(active) // if use `this`, remove both each functions
-     .addClass('active')
-   .siblings()
-     .removeClass('active');
-
-   }).filter('[href="#panel-signup"]')
-     .trigger('click');
-
- });
-});
