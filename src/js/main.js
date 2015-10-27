949189351b3eabe7e4446dd3ece9afcd0bae3111
Jasmine Baik <boshnivolo@gmail.com>
2015-10-27 17:22:53 -0400

    Establish angular structure for creating new users

--- src/js/main.js
+++ src/js/main.js
@@ -23,11 +23,24 @@
     });
 
 
-}); //END .MODULE
+})
+
+.controller("SignupController", function($scope, $http){
+     $scope.signup = {
+       username: "",
+       email: "",
+       password: ""
+     };
+
+     $scope.submit = function(){
+       $http.post('https://aqueous-sea-6980.herokuapp.com/api/users/', $scope.signup)
+         .then(function(){
+
+       });
+     };
+   }) // END LoginController
 
 
-
-
-
+   ; //END .MODULE
 
   })(); //END IIFE
